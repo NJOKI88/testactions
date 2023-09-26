2921ace44845ddecb96c8112a15a576ae923c56f
Njoki88 <tabbz08@yahoo.com>
2023-09-25 21:46:38 -0500

    second commit

--- .github/workflows/demo/work.tf
+++ .github/workflows/demo/work.tf
@@ -1,31 +1,46 @@
-name=  "Terraform"
+name = "Terraform"
 
-on=  [push]
+on = {
+  push = {}
+}
 
- env= "key"{
- }
- 
-     AWS_ACCESS_KEY_ID= $ {{ secrets.ACCESS_KEY}}
-     AWS_SECRET_KEY_ID= $ {{secrets.SECRET_KEY}}
-  
-jobs=
-  terraform=
-    runs-on=  ubuntu-latest
-    steps=
-      - name= Checkout
-        uses= actions/checkoutv3
-      - name= Setup terraform
-        uses= hashicorp/setup-terraformv2
-        with=
-          terraform_version: 1.1.9
-      - name= terraform init
-        run= terraform init        
-      - name= validate
-        run= terraform validate
-      - name= format
-        run= terraform fmt -check
-      - name= plan
-        run=  terraform plan
+env = {
+  AWS_ACCESS_KEY_ID     = "${{ secrets.ACCESS_KEY }}"
+  AWS_SECRET_ACCESS_KEY = "${{ secrets.SECRET_KEY }}"
+}
 
-
-
+jobs = {
+  terraform = {
+    runs-on = "ubuntu-latest"
+    
+    steps = [
+      {
+        name = "Checkout"
+        uses = "actions/checkout@v2"
+      },
+      {
+        name = "Setup Terraform"
+        uses = "hashicorp/setup-terraform@v1"
+        with = {
+          terraform_version = "1.1.9"
+        }
+      },
+      {
+        name = "Terraform Init"
+        run = "terraform init"
+      },
+      {
+        name = "Terraform Validate"
+        run = "terraform validate"
+      },
+      {
+        name = "Terraform Format Check"
+        run = "terraform fmt -check"
+      },
+      {
+        name = "Terraform Plan"
+        run = "terraform plan"
+      }
+    ]
+  }
+}
